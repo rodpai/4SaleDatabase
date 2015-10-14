2ddd70a49123be6b8925e5d45d33bb3e6551a9f7
Rodrigo Manuel Ferreira Paiva <r0da5@hotmail.com>
2015-10-14 23:14:59 +0100

    Begining

---
+++ 4SaleDatabase/USR/Tables/Users.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [USR].[Users] (
+    [IdUser]       NUMERIC (18) IDENTITY (1, 1) NOT NULL,
+    [Username]     VARCHAR (20) NOT NULL,
+    [Email]        VARCHAR (50) NOT NULL,
+    [PasswordSalt] VARCHAR (32) NOT NULL,
+    [Password]     VARCHAR (64) NOT NULL,
+    [CreationDate] DATETIME     NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([IdUser] ASC)
+);
+GO
+
+ALTER TABLE [USR].[Users] ADD CONSTRAINT UC_Usename UNIQUE (Username)
+GO
---
+++ 4SaleDatabase/dbo/Stored Procedures/pr_Users_Insert.sql
@@ -0,0 +1,37 @@
+﻿-- =============================================
+-- Author:		Rodrigo 
+-- Create date:	14/10/2015
+-- Description:	Insert
+-- =============================================
+CREATE PROCEDURE dbo.pr_Users_Insert
+(
+	@p_Username varchar (20),
+	@p_Email varchar (50),
+	@p_PasswordSalt varchar (10),
+	@p_Password varchar (64),
+	@p_CreationDate datetime,
+	@p_IdUser numeric (18,0) OUTPUT
+)
+AS
+Begin
+	SET NOCOUNT ON
+
+	INSERT INTO USR.Users
+	( 
+		Username, 
+		Email, 
+		PasswordSalt, 
+		[Password], 
+		CreationDate
+	)
+	VALUES
+	(
+		@p_Username,
+		@p_Email,
+		@p_PasswordSalt,
+		@p_Password,
+		@p_CreationDate
+	)
+
+	SET @p_IdUser = SCOPE_IDENTITY();
+End;
